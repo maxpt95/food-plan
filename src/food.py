e43d36266a6920332ce55f95613dbc8bdb28285b
Maximiliano Perez <maximiliano.pereztambasco@vancleefarpels.com>
2026-06-01 08:18:47 +0200

    feat: ServingSize validates servings are positive.
    Change ServingSize and NutritionalInfo to normal classes as adding setters and getters
    to dataclass gets funky.

--- src/food.py
+++ src/food.py
@@ -1,30 +1,67 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
 from copy import deepcopy
 
 
-# TODO: validate amount is positive
-@dataclass
 class ServingSize:
-    amount: float
-    unit: str
+    def __init__(self, amount: float, unit: str):
+        if amount < 0:
+            raise ValueError("Amount can't be negative")
+        self._amount = amount
+        self.unit = unit
+
+    @property
+    def amount(self) -> float:
+        return self.amount
+
+    @amount.setter
+    def amount(self, value: float):
+        """Sets only positive amounts."""
+        if self.amount < 0:
+            raise ValueError("Amount can't be negative")
+        self.amount = value
+
+    def __eq__(self, other: ServingSize) -> bool:
+        if not isinstance(other, ServingSize):
+            return False
+
+        return other._amount == self._amount and other.unit == self.unit
 
     def __str__(self):
         return f"Serving Size: {self.amount}{self.unit}"
 
 
-# TODO: validate macronutrients are positive
-@dataclass
+# TODO: validate setters check positive values
 class NutritionalInfo:
-    serving_size: ServingSize
-    fats: float
-    carbs: float
-    proteins: float
+    def __init__(
+        self, serving_size: ServingSize, fats: float, carbs: float, proteins: float
+    ):
+        """Validate macronutrients are positive."""
+        if fats < 0 or carbs < 0 or proteins < 0:
+            raise ValueError("Fats, carbs and proteins can't be negative")
+
+        self.serving_size = serving_size
+        self._fats = fats
+        self._carbs = carbs
+        self._proteins = proteins
 
     @classmethod
     def from_dict(cls, nutritional_info: dict):
         nutritional_info_cp = deepcopy(nutritional_info)
         serving_size = ServingSize(**nutritional_info_cp.pop("serving_size"))
         return cls(serving_size, **nutritional_info_cp)
+
+    def __eq__(self, other: NutritionalInfo) -> bool:
+        if not isinstance(other, NutritionalInfo):
+            return False
+
+        return (
+            other.serving_size == self.serving_size
+            and other._fats == self._fats
+            and other._carbs == self._carbs
+            and other._proteins == self._proteins
+        )
 
     def __str__(self):
         return f"""Nutritional Info
--- tests/test_food.py
+++ tests/test_food.py
@@ -1,20 +1,79 @@
 from food import NutritionalInfo, ServingSize
+from copy import deepcopy
 
-TORTILLA_NUTRITION = {
-    "serving_size": {"unit": "grams", "amount": 60},
-    "fats": 4,
-    "carbs": 27,
-    "proteins": 6,
-}
+import pytest
+
+
+@pytest.fixture
+def tortilla_nutrition_dict() -> dict:
+    return {
+        "serving_size": {"unit": "grams", "amount": 60},
+        "fats": 4,
+        "carbs": 27,
+        "proteins": 6,
+    }
+
+
+@pytest.fixture
+def tortilla_serving(tortilla_nutrition_dict: dict) -> ServingSize:
+    return ServingSize(**tortilla_nutrition_dict["serving_size"])
+
+
+@pytest.fixture
+def tortilla_nutrition(tortilla_nutrition_dict: dict) -> NutritionalInfo:
+    return NutritionalInfo.from_dict(tortilla_nutrition_dict)
+
+
+class TestServingSize:
+    def test_equal(self, tortilla_serving: ServingSize):
+        tortilla_serving_cp = deepcopy(tortilla_serving)
+
+        assert tortilla_serving_cp == tortilla_serving
+
+    def test_not_equal(self, tortilla_serving: ServingSize):
+        tortilla_serving_cp = deepcopy(tortilla_serving)
+        tortilla_serving_cp.amount = 999
+
+        assert tortilla_serving_cp != tortilla_serving
+
+    def test_init_negative_amount(self, tortilla_nutrition_dict: dict):
+        tortilla_nutrition_dict["serving_size"]["amount"] *= -1
+
+        with pytest.RaisesExc(ValueError, match="Amount can't be negative"):
+            ServingSize(**tortilla_nutrition_dict["serving_size"])
+
+    def test_set_negative_amount(self, tortilla_serving: ServingSize):
+        with pytest.RaisesExc(ValueError, match="Amount can't be negative"):
+            tortilla_serving.amount *= -1
 
 
 class TestNutritionalInfo:
-    def test_from_dict(self):
-        serving_size = ServingSize(**TORTILLA_NUTRITION["serving_size"])
+    def test_init_bad_macros(self, tortilla_nutrition_dict: dict):
+        """Test macronutrients fail if negative"""
+
+        tortilla_nutrition_dict["carbs"] *= -1
+        with pytest.RaisesExc(ValueError):
+            NutritionalInfo.from_dict(tortilla_nutrition_dict)
+
+    def test_equal(self, tortilla_nutrition: NutritionalInfo):
+        tortilla_nutrition_cp = deepcopy(tortilla_nutrition)
+
+        assert tortilla_nutrition == tortilla_nutrition_cp, "tortillas should be equal "
+
+    def test_not_equal(self, tortilla_nutrition: NutritionalInfo):
+        tortilla_nutrition_cp = deepcopy(tortilla_nutrition)
+        tortilla_nutrition_cp._fats = 999
+
+        assert tortilla_nutrition_cp != tortilla_nutrition, (
+            "tortillas shouldn't be equal"
+        )
+
+    def test_from_dict(self, tortilla_nutrition_dict: dict):
+        serving_size = ServingSize(**tortilla_nutrition_dict["serving_size"])
         nutrition = NutritionalInfo(
             serving_size,
-            fats=TORTILLA_NUTRITION["fats"],
-            carbs=TORTILLA_NUTRITION["carbs"],
-            proteins=TORTILLA_NUTRITION["proteins"],
+            fats=tortilla_nutrition_dict["fats"],
+            carbs=tortilla_nutrition_dict["carbs"],
+            proteins=tortilla_nutrition_dict["proteins"],
         )
-        assert nutrition == NutritionalInfo.from_dict(TORTILLA_NUTRITION)
+        assert nutrition == NutritionalInfo.from_dict(tortilla_nutrition_dict)
